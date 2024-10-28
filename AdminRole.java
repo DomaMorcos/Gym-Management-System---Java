967fbc8121706a83bf3742666a744ae5b49a66db
Mootassem0 <es-moatassem.emad2027@alexu.edu.eg>
2024-10-28 17:54:22 +0300

    Sixth change

--- AdminRole.java
+++ AdminRole.java
@@ -4,10 +4,32 @@
  */
 package lab4;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Doma & Moatassem
  */
 public class AdminRole {
-    
+    private TrainerDatabase database;
+
+    public AdminRole(TrainerDatabase database){
+        this.database = database;
+    }
+
+    public void addTrainer(Trainer trainer){
+        database.insertRecord(trainer);
+    }
+
+    public ArrayList<Record> getListOfTrainers () {
+        return database.returnAllRecords();
+    }
+
+    public void removeTrainer(String searchKey){
+        database.deleteRecord(searchKey);
+    }
+
+    public void logout() {
+
+    }
 }
