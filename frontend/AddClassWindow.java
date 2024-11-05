b735bad4c6e786164c51600a97d67e73dbfd1023
Mootassem0 <es-moatassem.emad2027@alexu.edu.eg>
2024-11-05 20:32:10 +0200

    Added Feautures1

--- frontend/AddClassWindow.java
+++ frontend/AddClassWindow.java
@@ -3,5 +3,16 @@ package frontend;
 import javax.swing.*;
 
 public class AddClassWindow {
-    private JPanel panel1;
+    private JPanel AddClassP;
+    private JTextField classIdField;
+    private JTextField classNameField;
+    private JLabel classIdLabel;
+    private JLabel classNameLabel;
+    private JTextField trainerIdField;
+    private JLabel trainerIdLabel;
+    private JTextField durationField;
+    private JLabel durationLabel;
+    private JTextField maxParticipantsField;
+    private JLabel maxParticipantsLabel;
+    private JButton addButton;
 }
