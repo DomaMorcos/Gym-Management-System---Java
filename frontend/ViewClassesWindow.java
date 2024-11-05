9fc6520ed078c2b094a213ff02578ba9adab4554
Mootassem0 <es-moatassem.emad2027@alexu.edu.eg>
2024-11-05 20:54:02 +0200

    Added Feautures2

--- frontend/ViewClassesWindow.java
+++ frontend/ViewClassesWindow.java
@@ -1,4 +1,10 @@
 package frontend;
 
+import javax.swing.*;
+
 public class ViewClassesWindow {
+    private JPanel ViewClassesP;
+    private JTable classesTable;
+    private JButton backButton;
+    private JScrollPane tableScrollPane;
 }
